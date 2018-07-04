1aa74dc325097140cbbb24e0fb95cf3c6a98ba00
Laurent Stephenson <laurent.stephenson@gmail.com>
2018-07-04 23:45:36 +0100

    packing/unpacking fn for annoying curve_fit

--- gaussian_beam.py
+++ gaussian_beam.py
@@ -1,6 +1,32 @@
 
 import numpy as np
 from scipy.optimize import curve_fit
+from image_tools import derived_results
+
+
+def pack(args):
+    """packs parameters given as scalars into parameter dict"""
+    p = {}
+    p['mean'] = np.array(args[0:2])
+    p['cov'] = np.empty((2,2))
+    p['cov'][0,:] = args[2:4]
+    p['cov'][1,:] = args[3:5]
+    p['scale'] = args[5]
+    p['offset'] = args[6]
+
+    return p
+
+
+def unpack(p):
+    """unpack parameter dict into individual parameters"""
+    args = np.ones(7)
+    args[0:2] = p['mean']
+    args[2:4] = p['cov'][0,:]
+    args[4] = p['cov'][1,1]
+    args[5] = p['scale']
+    args[6] = p['offset']
+
+    return args
 
 
 def parameter_initialiser(x, y):
@@ -17,37 +43,53 @@ def parameter_initialiser(x, y):
 
     cov = np.einsum("ijk,ij,ijl->kl", d, prob, d)
 
-    return x0[0], x0[1], cov[0,0], cov[0,1], cov[1,1], a, y0
+    p = {}
+    p['mean'] = x0
+    p['cov'] = cov
+    p['scale'] = a
+    p['offset'] = y0
+
+    return p
 
 
-# annoying function signature
-def fitting_function(x, x0_0, x0_1, c00, c01, c11, a, y0):
-    x0 = np.array([x0_0, x0_1])
+def _fitting_function(x, p):
+    x0 = p['mean']
+    cov = p['cov']
+    scale = p['scale']
+    y0 = p['offset']
+
     x = np.einsum("i...->...i", x)
     d = x - x0
 
     # these operations can be done with numpy.linalg, but that's probably very
     # slow. our covariance matrix is symmetric by definition and can easily be
     # inverted manually
-    det = c00*c00 - c01*c01
+    det = cov[0,0]*cov[0,0] - cov[0,1]*cov[0,1]
     inv = np.zeros((2,2))
-    inv[0,0] = c11/det
-    inv[0,1] = -c01/det
+    inv[0,0] = cov[1,1]/det
+    inv[0,1] = -cov[0,1]/det
     inv[1,0] = inv[0,1]
-    inv[1,1] = c00/det
+    inv[1,1] = cov[0,0]/det
 
     pref = (1/(2*np.pi*np.sqrt(np.abs(det))))
     expo = -0.5*np.einsum("...k,kl,...l->...", d, inv, d)
 
-    y = a*pref*np.exp(expo) + y0
+    y = scale*pref*np.exp(expo) + y0
 
     return y
+
+
+# annoying function signature for curve_fit
+def fitting_function(x, x0_0, x0_1, c00, c01, c11, a, y0):
+    p = pack([x0_0, x0_1, c00, c01, c11, a, y0])
+    return _fitting_function(x, p)
 
 
 class gaussian_beam:
     def fit(self, xdata, ydata, p0_dict=None, **kwargs):
         if p0_dict is None:
-            p0 = parameter_initialiser(xdata, ydata)
+            p0 = unpack(parameter_initialiser(xdata, ydata))
+            print(pack(p0))
         else:
             p0 = None
             # p0 = np.zeros(4)
@@ -61,4 +103,5 @@ class gaussian_beam:
         xdata = xdata.reshape(2, m*n)
         ydata = ydata.reshape(m*n)     #just flatten
 
-        return curve_fit(fitting_function, xdata, ydata, p0=p0)
+        p_fit, p_err = curve_fit(fitting_function, xdata, ydata, p0=p0)
+        return pack(p_fit)

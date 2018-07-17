74a7c80ea968f89435d11d7b666167e57ca63d99
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2018-07-17 21:44:26 +0100

    cjb style camera streamer

---
+++ video_example.py
@@ -0,0 +1,25 @@
+"""An example of how to take a continuous stream of images using the AndorEmccd class"""
+import time
+import matplotlib.pyplot as plt
+from camera_controller.camera_controller import ThorlabsCCD
+print("imported", flush=True)
+
+cam = ThorlabsCCD()
+print("connected!", flush=True)
+
+cam.set_image_region(0,512, 260,300)
+print("set region")
+cam.set_exposure_time(0.01)
+print("set exposure", flush=True)
+
+cam.start_acquisition()
+print("starting acquisition", flush=True)
+
+n_acquired = 0
+t_start = time.time()
+while n_acquired < 5:
+    imVec = cam.get_all_images()
+    if imVec is None:
+        continue
+    n_acquired += len(imVec)
+print("n_acquired = {}. Frame rate = {} /s".format(n_acquired, n_acquired/(time.time()-t_start)))

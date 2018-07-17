b9e0a287c40339ad888f01b4676636a3914422e3
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2018-07-17 21:41:34 +0100

    new style controller

--- camera_controller/camera_controller.py
+++ camera_controller/camera_controller.py
@@ -1,9 +1,8 @@
-
-
-
 from . import uc480
+# print("imported uc480", flush=True)
 import numpy as np
 import ctypes
+import collections
 import time
 
 from threading import Thread
@@ -43,102 +42,119 @@ def timeout(timeout):
     return deco
 
 
-class CameraController:
+class ThorlabsCCD:
     # Servo polling period in seconds
-    _POLL_PERIOD = 0.01
+    _POLL_PERIOD = 0.05
 
-    def __init__(self):
-
-        self.c = uc480.uc480()
+    def __init__(self, id_=None, framebuffer_len=100):
+        self.camera_id = id_ if id_ is not None else 0
+        self._library_init()
 
         self.quit = False
+        self.dead = False
         self.connected = False
 
-        # Does this controller automatically acquire images?
         self.acquisition_enabled = False
 
-        self._image = None
-        self.new_image = False
-
         self.auto_exposure = True
-        self.exposure = 0
-        self.exposure_min = 0
-        self.exposure_max = 1
-        self.exposure_inc = 0.1
+        self.exposure = None
+        self.exposure_min = None
+        self.exposure_max = None
+        self.exposure_inc = None
 
-    def _acquisition_thread_exec(self):
+        self._frame_buffer = collections.deque([], framebuffer_len)
+        self._frame_call_list = []
 
-        while self.quit is False:
+        # connect to camera
+        self._connect()
 
-            if self.acquisition_enabled:
-                self.acquire()
+        # Start image acquisition thread
+        t = Thread(target=self._acquisition_thread, daemon=True)
+        t.start()
 
-            time.sleep(self._POLL_PERIOD)
+    def _library_init(self):
+        self.c = uc480.uc480()
 
-    def get_n_cameras(self):
+    def _library_cleanup(self):
+        del self.c._lib
+
+    def _connect(self):
+        """Connect to camera"""
+        self.c.connect(self.camera_id)
+        self._get_sensor_info()
+        self._get_exposure_params()
+        self._get_aoi()
+        self.connected = True
+
+    def _disconnect(self):
+        self.c.disconnect()
+        self.connected = False
+
+    def _reconnect(self):
+        self._disconnect()
+        self._library_cleanup()
+        self._library_init()
+        self._connect()
+
+    def _get_n_cameras(self):
         """Returns the number of cameras connected to the system"""
         n_cams = ctypes.c_int()
         self.c.call("is_GetNumberOfCameras", ctypes.pointer(n_cams))
         return n_cams.value
 
-    def acquire(self):
+    def _acquisition_thread(self):
+        while self.quit is False:
+            if self.acquisition_enabled:
+                try:
+                    # print("waiting", flush=True)
+                    im = timeout(1)(self.c.acquire)(native=True)
+                    # print("acquired!", flush=True)
+                except (MyTimeoutError, uc480.uc480Error) as e:
+                    self._reconnect()
+                else:
+                    im = self._crop_to_aoi(im)
+                    self._frame_buffer.append(im)
+                    for f in self._frame_call_list:
+                        f(im)
 
-        n_cams = self.get_n_cameras()
+                    # print("enabled {}".format(self.acquisition_enabled), flush=True)
+                    # Update the exposure if necessary
+                    if self.auto_exposure:
+                        # print("try auto", flush=True)
+                        timeout(1)(self.handle_auto_exposure)(im)
 
-        if n_cams == 0:
-            return
+            # print("sleeping", flush=True)
+            time.sleep(self._POLL_PERIOD)
+            # print("slept", flush=True)
+        self.dead = True
 
-        if self.c._camID is None:
-            self.connect()
-
-        try:
-            self._image = timeout(1)(self.c.acquire)(native=True)
-            self.new_image = True
-
-            # Update the exposure if necessary
-            if self.auto_exposure:
-                 timeout(1)(self.handle_auto_exposure)()
-
-        except (MyTimeoutError, uc480.uc480Error) as e:
-            print("Disconnected from camera")
-            self.disconnect()
-            print("Cleaning up library")
-            del self.c._lib
-            print("Restarting library")
-            self.c = uc480.uc480()
-
-    def connect(self):
-        self.c.connect()
-        self.exposure = self.c.get_exposure()
-        self.exposure_min, self.exposure_max, \
-            self.exposure_inc = self.c.get_exposure_limits()
-        self.connected = True
-
-    def disconnect(self):
-        self.c.disconnect()
-        self.connected = False
-
-    def ping(self):
-        return True
-
-    def start_acquisition(self):
+    def start_acquisition(self, single=False):
         """Turn on auto acquire"""
+        def acquire_single_cb(_):
+            """image is passed to callback and not used"""
+            self.stop_acquisition()
+            self.deregister_callback(acquire_single_cb)
+
+        if single:
+            self.register_callback(acquire_single_cb)
         self.acquisition_enabled = True
 
     def stop_acquisition(self):
         """Turn off auto acquire"""
         self.acquisition_enabled = False
 
+    def _get_exposure_params(self):
+        self.exposure = self.c.get_exposure()
+        self.exposure_min, self.exposure_max, \
+            self.exposure_inc = self.c.get_exposure_limits()
 
-    def close(self):
-        self.quit = True
+    def set_exposure_time(self, exposure_time):
+        """Set the CCD exposure time in seconds"""
+        time_ms = exposure_time*1000
+        self.set_exposure_ms(time_ms)
 
-    def get_image(self):
-        """Returns last image"""
-        return self._image
-
-    def set_exposure(self, exposure_time):
-        """Set the camera exposure time is ms"""
+    def set_exposure_ms(self, exposure_time):
+        """Set the camera exposure time in ms"""
         self.c.set_exposure(exposure_time)
         self.exposure = exposure_time
 
@@ -146,32 +162,137 @@ class CameraController:
         """Enable/disable auto exposure"""
         self.auto_exposure = auto_exposure
 
-    def handle_auto_exposure(self):
+    def handle_auto_exposure(self, image):
         """Reads the most recent image and updates exposure"""
-        pixel_max = np.max(self._image)
+        # print("got to exposure handler", flush=True)
+        pixel_max = np.amax(image)
 
+        # print("autoexpose", flush=True)
         if pixel_max > auto_exposure_max_threshold:
-            exposure = self.exposure*0.9
-            if exposure < self.exposure_min:
-                exposure = self.exposure_min
-            self.set_exposure(exposure)
+            exposure = np.maximum(self.exposure*0.9, self.exposure_min)
+            # if exposure == self.exposure:
+                # print("warning: min exposure reached", flush=True)
+            # print("reducing exposure", flush=True)
+            self.set_exposure_ms(exposure)
 
         elif pixel_max < auto_exposure_min_threshold:
-            exposure = self.exposure*1.1
-            if exposure > self.exposure_max:
-                exposure = self.exposure_max
-            self.set_exposure(exposure)
+            exposure = np.minimum(self.exposure*1.1, self.exposure_max)
+            # if exposure == self.exposure:
+                # print("warning: max exposure reached", flush=True)
+            # print("increasing exposure", flush=True)
+            self.set_exposure_ms(exposure)
 
+    def set_image_region(self, hStart, hEnd, vStart, vEnd, **kwargs):
+        """Set the CCD region to read out.
+        The region is 0 indexed and inclusive, so the valid ranges for hStart
+        is 0 ... self.ccd_width - 1 etc."""
+        if kwargs:
+            print("binning is not supported")
+        self.aoi_x = self._clip_to_grid(int(vStart), 0, self.ccd_height, 2)
+        self.aoi_y = self._clip_to_grid(int(hStart), 0, self.ccd_width, 4)
+        self.aoi_width = self._clip_to_grid(int(1+vEnd-vStart), 4, self.ccd_height, 2)
+        self.aoi_height = self._clip_to_grid(int(1+hEnd-hStart), 32, self.ccd_width, 4)
 
+        self._set_aoi(self.aoi_x, self.aoi_y, self.aoi_height, self.aoi_width)
+        # exposure settings will change
+        self._get_exposure_params()
 
+    def _clip_to_grid(self, in_val, min_, max_, grid):
+        out_val = np.clip(in_val, min_, max_)
+        # integer maths does the rounding
+        out_val = out_val // grid * grid
+        return out_val
 
+    def _set_aoi(self, posx, posy, width, height, absolute=True):
+        """Set Area Of Interest according to Thorlabs spec
+        If absolute is True, the memory location of the aoi within the image
+        matches its actual position
+        """
+        rectAOI = uc480.IS_RECT()
+        rectAOI.s32X = posx
+        rectAOI.s32Y = posy
+        rectAOI.s32Width = width
+        rectAOI.s32Height = height
 
-def start():
-    import threading
-    dev = CameraController()
+        self.aoi_absolute = absolute
+        if self.aoi_absolute:
+            rectAOI.s32X |= uc480.IS_AOI_IMAGE_POS_ABSOLUTE
+            rectAOI.s32Y |= uc480.IS_AOI_IMAGE_POS_ABSOLUTE
 
-    t = threading.Thread(target=dev._acquisition_thread_exec)
-    t.daemon = True
-    t.start()
-    return dev
+        self.c.call("is_AOI", self.c._camID, uc480.IS_AOI_IMAGE_SET_AOI,
+            ctypes.pointer(rectAOI), ctypes.sizeof(rectAOI))
 
+    def _get_aoi(self):
+        """Gets the aoi parameters, sets internal values and returns them"""
+        rectAOI = uc480.IS_RECT()
+        self.c.call("is_AOI", self.c._camID, uc480.IS_AOI_IMAGE_GET_AOI,
+            ctypes.pointer(rectAOI), ctypes.sizeof(rectAOI))
+
+        self.aoi_width = rectAOI.s32Width
+        self.aoi_height = rectAOI.s32Height
+
+        # the following is agnostic of whether the aoi mode is absolute or not
+        self.aoi_x = rectAOI.s32X & ~uc480.IS_AOI_IMAGE_POS_ABSOLUTE
+        self.aoi_y = rectAOI.s32Y & ~uc480.IS_AOI_IMAGE_POS_ABSOLUTE
+
+        return self.aoi_x, self.aoi_y, self.aoi_width, self.aoi_height
+
+    def _crop_to_aoi(self, image):
+        """crop the image to the size of the aoi"""
+        if self.aoi_width == self.ccd_width \
+                and self.aoi_height == self.ccd_height:
+            # shortcut for when the aoi is the whole sensor
+            return image
+
+        x_start = self.aoi_x if self.aoi_absolute else 0
+        y_start = self.aoi_y if self.aoi_absolute else 0
+        x_end = self.aoi_width + x_start
+        y_end = self.aoi_height + y_start
+        return image[y_start:y_end, x_start:x_end]
+
+    def _get_sensor_info(self):
+        # get sensor info
+        pInfo = uc480.SENSORINFO()
+        self.c.call("is_GetSensorInfo", self.c._camID, ctypes.pointer(pInfo))
+        self.ccd_width = pInfo.nMaxWidth
+        self.ccd_height = pInfo.nMaxHeight
+
+    def register_callback(self, f):
+        """Register a function to be called from the acquisition thread for each
+        new image"""
+        self._frame_call_list.append(f)
+
+    def deregister_callback(self, f):
+        if f in self._frame_call_list:
+            self._frame_call_list.remove(f)
+
+    def get_image(self):
+        """Returns the oldest image in the buffer as a numpy array, or None if
+        no new images"""
+        if len(self._frame_buffer) == 0:
+            return None
+        return self._frame_buffer.popleft()
+
+    def get_all_images(self):
+        """Returns all of the images in the buffer as an array of numpy arrays,
+        or None if no new images"""
+        if len(self._frame_buffer):
+            ims = []
+            while len(self._frame_buffer) > 0:
+                ims.append(self._frame_buffer.popleft())
+        else:
+            ims = None
+        return ims
+
+    def ping(self):
+        return True
+
+    def close(self):
+        self.quit = True
+        while not self.dead:
+            time.sleep(0.01)
+        self._disconnect()
+        self._library_cleanup()
+
+    def __del__(self):
+        self.close()

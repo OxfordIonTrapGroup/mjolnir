7c1a46f97dc0757616778d124ad8820006b6f34e
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2018-11-29 17:31:12 +0000

    new_image_tools: change methods to class methods

--- new_image_tools.py
+++ new_image_tools.py
@@ -106,32 +106,45 @@ def fitting_function(x, x0_0, x0_1, c00, c01, c11, a, y0):
 
 
 class GaussianBeam:
-    def naive_fit(self, xdata, ydata):
+    @classmethod
+    def f(cls, xdata, p):
+        return _fitting_function(xdata, p)
+
+    @classmethod
+    def naive_fit(cls, xdata, ydata):
         return parameter_initialiser(xdata, ydata)
 
-    def two_step_fit(self, xdata, ydata, region=50):
+    @classmethod
+    def two_step_MLE_mask(cls, xdata, ydata, region=50):
+        """Uses MLE first on full data, then on data masked by initial fit"""
         p = parameter_initialiser(xdata, ydata)
         hr = int(region/2)
 
-        ii,jj = np.around(p['x0']).astype(int)
-        mask = np.full_like(ydata, False, dtype=bool)
-        mask[ii-hr:ii+hr, jj-hr:jj+hr] = True
+        masked = cls._mask(xdata, ydata, p['x0'], region=region)
+        # ii,jj = np.around(p['x0']).astype(int)
+        # mask = np.full_like(ydata, False, dtype=bool)
+        # mask[ii-hr:ii+hr, jj-hr:jj+hr] = True
 
-        masked = np.copy(ydata)
-        masked[~mask] = 0.
+        # masked = np.copy(ydata)
+        # masked[~mask] = 0.
         return parameter_initialiser(xdata, masked)
 
-    def two_step_fit_mk2(self, xdata, ydata, region=50):
+    @classmethod
+    def two_step_MLE(cls, xdata, ydata, region=50):
+        """Uses MLE first on full data, then on data cropped by initial fit"""
         p = parameter_initialiser(xdata, ydata)
-        hr = int(region/2)
 
-        ii,jj = np.around(p['x0']).astype(int)
-        xcrop = xdata[:, ii-hr:ii+hr, jj-hr:jj+hr]
-        ycrop = ydata[ii-hr:ii+hr, jj-hr:jj+hr]
+        xcrop, ycrop = cls._crop(xdata, ydata, p['x0'], region=region)
+        # hr = int(region/2)
+        # ii,jj = np.around(p['x0']).astype(int)
+        # xcrop = xdata[:, ii-hr:ii+hr, jj-hr:jj+hr]
+        # ycrop = ydata[ii-hr:ii+hr, jj-hr:jj+hr]
 
         return parameter_initialiser(xcrop, ycrop)
 
-    def lsq_fit(self, xdata, ydata, p0_dict=None, **kwargs):
+    @classmethod
+    def lsq_fit(cls, xdata, ydata, p0_dict=None, **kwargs):
+        """Least squares fit on all data points"""
         if p0_dict is None:
             p0 = unpack(parameter_initialiser(xdata, ydata))
         else:
@@ -144,31 +157,37 @@ class GaussianBeam:
         p_fit, p_err = curve_fit(fitting_function, xdata, ydata, p0=p0)
         return pack(p_fit)
 
-    def lsq_cropped(self, xdata, ydata, region=50):
+    @classmethod
+    def lsq_cropped(cls, xdata, ydata, region=50):
+        """Crops data using MLE fit, then uses least squares on result"""
         p = parameter_initialiser(xdata, ydata)
-        hr = int(region/2)
 
-        ii,jj = np.around(p['x0']).astype(int)
-        xcrop = xdata[:, ii-hr:ii+hr, jj-hr:jj+hr]
-        ycrop = ydata[ii-hr:ii+hr, jj-hr:jj+hr]
+        xcrop, ycrop = cls._crop(xdata, ydata, p['x0'], region=region)
+        # hr = int(region/2)
+        # ii,jj = np.around(p['x0']).astype(int)
+        # xcrop = xdata[:, ii-hr:ii+hr, jj-hr:jj+hr]
+        # ycrop = ydata[ii-hr:ii+hr, jj-hr:jj+hr]
 
         return self.lsq_fit(xcrop, ycrop, p0_dict=p)
 
-    def _get_limits(self, x0, shape, region=50):
+    @classmethod
+    def _get_limits(cls, x0, shape, region=50):
         """returns imin, imax, jmin, jmax"""
         hr = int(region/2)
         x0 = np.around(x0).astype(int)
         lims = np.array([x0-hr, x0+hr])
-        return np.clip(lims, [0,0], np.array(shape)-1).flatten()
+        return np.clip(lims, [0,0], np.array(shape)-1).T.flatten()
 
-    def _crop(self, xdata, ydata, x0, region=50):
-        imin, imax, jmin, jmax = self._get_limits(x0, ydata.shape, region)
+    @classmethod
+    def _crop(cls, xdata, ydata, x0, region=50):
+        imin, imax, jmin, jmax = cls._get_limits(x0, ydata.shape, region)
         xcrop = xdata[:, imin:imax, jmin:jmax]
         ycrop = ydata[imin:imax, jmin:jmax]
         return xcrop, ycrop
 
-    def _mask(self, ydata, x0, region=50):
-        imin, imax, jmin, jmax = self._get_limits(x0, ydata.shape, region)
+    @classmethod
+    def _mask(cls, ydata, x0, region=50):
+        imin, imax, jmin, jmax = cls._get_limits(x0, ydata.shape, region)
         mask = np.full_like(ydata, False, dtype=bool)
         mask[imin:imax, jmin:jmax] = True
         ymasked = np.copy(ydata)

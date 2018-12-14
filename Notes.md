ca8d05a08b8705c48eace203838ee8ae96cae241
Laurent Stephenson <laurent.stephenson@physics.ox.ac.uk>
2018-12-14 21:48:51 +0000

    notes and installer reflecting working way of building

--- Notes.md
+++ Notes.md
@@ -21,3 +21,16 @@ Failed to install python properly so I had some issues getting the DLL loaded.
 This is the install location so I don't have to look for it again.
 
 `C:\Program Files\Thorlabs\Scientific Imaging\uc480_64.dll`
+
+
+### Building with PyInstaller
+
+Didn't look hard enough at what was supported when trying to build.
+
+~~
+PyInstaller 3.4 (9 Sep 2018 release) isn't compatible with Python 3.7 at this
+point in time, so pip installed from github, develop branch, at this commit:
+https://github.com/pyinstaller/pyinstaller/commit/5edb4f7dee9c21f77c4a89342eb98248fe4c45b9
+~~
+
+Use Python 3.6 and PyInstaller 3.4, and it should 'just work'.
--- make_exe.sh
+++ make_exe.sh
@@ -1,1 +1,9 @@
-pyinstaller mjolnir/frontend/launcher.py -y -d --onefile --windowed --exclude-module matplotlib --paths $VIRTUAL_ENV/Lib/site-packages/PyQt5/Qt/bin --paths $VIRTUAL_ENV/Lib/site-packages/Library/bin --paths $VIRTUAL_ENV/Lib/site-packages
+pyinstaller mjolnir/test/gui_test.py -y -d all --clean --onefile --windowed
+#pyinstaller mjolnir/frontend/launcher.py -y --clean --onefile --windowed
+
+# These aren't needed any more
+# --exclude-module matplotlib
+# --paths $VIRTUAL_ENV/Lib/site-packages/PyInstaller
+# --paths $VIRTUAL_ENV/Lib/site-packages/PyQt5/Qt/bin  \
+# --paths $VIRTUAL_ENV/Lib/site-packages/Library/bin   \
+# --paths $VIRTUAL_ENV/Lib/site-packages
